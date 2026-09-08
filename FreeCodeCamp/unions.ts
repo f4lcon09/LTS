3f72695add0e2f8e5c0e49518bfed99c58bb35e6
f4lcon09 <vinicios.cristofolini2005@gmail.com>
2026-09-08 17:12:54 -0300

    Adicionado novas funções para consultar posteriormente

--- FreeCodeCamp/unions.ts
+++ FreeCodeCamp/unions.ts
@@ -6,6 +6,33 @@ score = "55"
 
 type User = {
     name: string;
+    id: number
 }
+
+type Admin = {
+    username: string;
+    id: number
+}
+
+let vinicios: User | Admin = {name: "Vinicios", id: 404}
+
+vinicios = {username: "VC", id: 404}
+
+// function getDbId(id: number | string){
+//     // making some API calls
+//     console.log(`DB id is: ${id}`)
+
+// }
+
+getDbId(3)
+getDbId("3")
+
+ function getDbId(id: number | string){
+     if (typeof id === "string"){
+        id.toLowerCase
+     }
+
+}
+
 
 export {}

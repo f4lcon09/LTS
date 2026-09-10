db54c414033814d5c322e5b16921b147f5c2a771
f4lcon09 <vinicios.cristofolini2005@gmail.com>
2026-09-10 15:57:05 -0300

    Inicio de aprendizado html com lite e live server

---
+++ PureTs/src/index.ts
@@ -0,0 +1,12 @@
+class User {
+    email: string
+    name: string
+    city: string = ""
+    constructor(email: string, name: string){
+        this.email = email;
+        this.name = name
+    }
+}
+
+const vinicios = new User("v@v.com", "Vinicios")
+vinicios.city = "camboriu"

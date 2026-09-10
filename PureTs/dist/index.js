46c943c06c035e4e71239da3c00f3b3a29416027
f4lcon09 <vinicios.cristofolini2005@gmail.com>
2026-09-10 18:00:46 -0300

    Mais algumas funções novas para o uso no index dentro do console do html

--- PureTs/dist/index.js
+++ PureTs/dist/index.js
@@ -1,10 +1,19 @@
 "use strict";
+// class User {
+//     public email: string
+//     private name: string // #name funciona igual
+//     readonly city: string = "camboriu"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
 class User {
     constructor(email, name) {
-        this.city = "";
         this.email = email;
         this.name = name;
+        this.city = "camboriu";
     }
 }
 const vinicios = new User("v@v.com", "Vinicios");
-vinicios.city = "camboriu";
+// vinicios.name
--- PureTs/src/index.ts
+++ PureTs/src/index.ts
@@ -1,12 +1,23 @@
+// class User {
+//     public email: string
+//     private name: string // #name funciona igual
+//     readonly city: string = "camboriu"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
 class User {
-    email: string
-    name: string
-    city: string = ""
-    constructor(email: string, name: string){
-        this.email = email;
-        this.name = name
+  
+    readonly city: string = "camboriu"
+    constructor(public email: string, 
+                public name: string,
+                // private userID:  string
+                ){
+
     }
 }
 
 const vinicios = new User("v@v.com", "Vinicios")
-vinicios.city = "camboriu"
+// vinicios.name
